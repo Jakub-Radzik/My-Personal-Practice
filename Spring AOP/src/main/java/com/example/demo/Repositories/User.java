74f66ec232e224c6d0d7980f1c5b3d8b989d1dad
Jakub Radzik <42437524+radzikoska123@users.noreply.github.com>
2021-05-27 12:01:35 +0200

    Add files via upload

---
+++ Spring AOP/src/main/java/com/example/demo/Repositories/User.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class User {
+    public String getUserDetails(){
+        return "user details";
+    }
+}
